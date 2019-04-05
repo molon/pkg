4746a47ceb81925864fbf9063fee5fc8a00d5166
molon <dudl@qq.com>
2019-04-05 23:44:59 +0800

    pb ts

--- util/proto.go
+++ util/proto.go
@@ -1,8 +1,12 @@
 package util
 
 import (
+	"time"
+
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	tspb "github.com/golang/protobuf/ptypes/timestamp"
 )
 
 func ProtoToJSONStringForPrint(pb proto.Message) string {
@@ -12,4 +16,18 @@ func ProtoToJSONStringForPrint(pb proto.Message) string {
 	}
 	str, _ := marshaler.MarshalToString(pb)
 	return str
+}
+
+func ToTimestampProto(t time.Time) (*tspb.Timestamp, error) {
+	if t.IsZero() {
+		return nil, nil
+	}
+	return ptypes.TimestampProto(t)
+}
+
+func FromTimestampProto(ts *tspb.Timestamp) (time.Time, error) {
+	if ts == nil {
+		return time.Time{}, nil
+	}
+	return ptypes.Timestamp(ts)
 }
