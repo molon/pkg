77da43a473bcc55fc8219402a1e2e0dcc8bb5dec
molon <dudl@qq.com>
2019-04-08 03:03:19 +0800

    add errors.WithErrorHandler

--- errors/grpc.go
+++ errors/grpc.go
@@ -9,16 +9,44 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+type interceptorOptions struct {
+	errHandler func(err error) error
+}
+
+type InterceptorOption func(*interceptorOptions)
+
+func WithErrorHandler(errHandler func(err error) error) InterceptorOption {
+	return func(options *interceptorOptions) {
+		options.errHandler = errHandler
+	}
+}
+
+func UnaryServerInterceptor(options ...InterceptorOption) grpc.UnaryServerInterceptor {
+	opts := &interceptorOptions{}
+	for _, option := range options {
+		option(opts)
+	}
+
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		resp, err := handler(ctx, req)
+		if opts.errHandler != nil {
+			err = opts.errHandler(err)
+		}
 		return resp, Cause(err)
 	}
 }
 
-func StreamServerInterceptor() grpc.StreamServerInterceptor {
+func StreamServerInterceptor(options ...InterceptorOption) grpc.StreamServerInterceptor {
+	opts := &interceptorOptions{}
+	for _, option := range options {
+		option(opts)
+	}
+
 	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		err := handler(srv, stream)
+		if opts.errHandler != nil {
+			err = opts.errHandler(err)
+		}
 		return Cause(err)
 	}
 }
