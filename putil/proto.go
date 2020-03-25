2e512e75040f403b3c0056485103580b190685ad
molon <dudl@qq.com>
2020-03-25 13:57:00 +0800

    add pb any methods

--- putil/proto.go
+++ putil/proto.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
 	tspb "github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/molon/pkg/errors"
 )
 
 func ProtoToJSONStringForPrint(pb proto.Message) string {
@@ -34,10 +35,27 @@ func FromTimestampProto(ts *tspb.Timestamp) (time.Time, error) {
 	return ptypes.Timestamp(ts)
 }
 
-func MarshalAny(pb proto.Message) (*any.Any, error) {
-	value, err := proto.Marshal(pb)
-	if err != nil {
-		return nil, err
+func UnmarshalAny(a *any.Any) (proto.Message, error) {
+	detail := &ptypes.DynamicAny{}
+	if err := ptypes.UnmarshalAny(a, detail); err != nil {
+		return nil, errors.WithStack(err)
 	}
-	return &any.Any{TypeUrl: "/" + proto.MessageName(pb), Value: value}, nil
+	return detail.Message, nil
 }
+
+func MarshalAny(pb proto.Message) (*any.Any, error) {
+	a, err := ptypes.MarshalAny(pb)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return a, nil
+}
+
+// 和前者的区别其实只是移除了type前缀
+// func MarshalAny(pb proto.Message) (*any.Any, error) {
+// 	value, err := proto.Marshal(pb)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &any.Any{TypeUrl: "/" + proto.MessageName(pb), Value: value}, nil
+// }
