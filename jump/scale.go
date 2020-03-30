7d3fca8477879fbf50884a758b0bbfee01a931d5
molon <dudl@qq.com>
2020-03-30 20:13:09 +0800

    IndexOfScalers fix

--- jump/scale.go
+++ jump/scale.go
@@ -1,21 +1,21 @@
 package jump
 
-type Scale interface {
-	Scale() int
+type Scaler interface {
+	GetScale() int
 }
 
-func IndexOfScales(scales []Scale, key uint64) int {
+func IndexOfScalers(scales []Scaler, key uint64) int {
 	bucket := 0
 	for _, s := range scales {
-		bucket += s.Scale()
+		bucket += s.GetScale()
 	}
 
 	idx := Hash(key, bucket)
 
 	p := 0
 	for i, s := range scales {
-		scale := s.Scale()
-		if idx >= p && idx < p+scale {
+		scale := s.GetScale()
+		if idx >= int32(p) && idx < int32(p+scale) {
 			return i
 		}
 		p += scale
