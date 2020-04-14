8a1b605937c838bf9fbde2e8c7569fd8a768fd05
molon <dudl@qq.com>
2020-04-15 03:28:55 +0800

    TimeFromUnixMilli

--- putil/time.go
+++ putil/time.go
@@ -11,8 +11,12 @@ func TimeUnixMilli(t time.Time) int64 {
 	return t.UnixNano() / int64(time.Millisecond)
 }
 
-func TimeNowUnixMilli() int64 {
+func TimeUnixMilliNow() int64 {
 	return TimeUnixMilli(time.Now())
+}
+
+func TimeFromUnixMilli(u int64) time.Time {
+	return time.Unix(0, u*int64(time.Millisecond))
 }
 
 func Sleep(ctx context.Context, d time.Duration) error {
