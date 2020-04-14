6779a91b8d750dfdec94fb7b532113b9832af9fb
molon <dudl@qq.com>
2020-04-15 03:07:36 +0800

    TimeNowUnixMilli and TimeUnixMilli

--- putil/time.go
+++ putil/time.go
@@ -7,6 +7,14 @@ import (
 	"github.com/molon/pkg/errors"
 )
 
+func TimeUnixMilli(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
+func TimeNowUnixMilli(t time.Time) int64 {
+	return TimeUnixMilli(time.Now())
+}
+
 func Sleep(ctx context.Context, d time.Duration) error {
 	t := time.NewTimer(d)
 	select {
